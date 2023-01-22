981d2009f4dacdc3cb399006d13934146529bbfc
Maciej Uzarski <s25527@pjwstk.edu.pl>
2023-01-22 17:55:25 +0100

    failed attempt at integration test

---
+++ src/test/java/com/example/bank/BankAccountControllerIT.java
@@ -0,0 +1,44 @@
+package com.example.bank;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.net.http.HttpHeaders;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = S25527bankApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BankAccountControllerIT {
+    @LocalServerPort
+    private int port;
+
+    TestRestTemplate restTemplate = new TestRestTemplate();
+
+    HttpHeaders headers = new HttpHeaders();
+
+    @Test
+    public void testGetBankAccounts() throws JSONException {
+
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/bank/accounts"),
+                HttpMethod.GET, entity, String.class);
+
+        String expected = "{\"id\":\"Course1\",\"name\":\"Spring\",\"description\":\"10 Steps\"}";
+
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
+}
