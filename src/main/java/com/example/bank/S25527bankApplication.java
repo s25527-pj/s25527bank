27870a258c046c9cf6c20a3124c5eec356ad19f0
Maciej Uzarski <s25527@pjwstk.edu.pl>
2023-01-22 15:28:51 +0100

    first commit

---
+++ src/main/java/com/example/bank/S25527bankApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S25527bankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S25527bankApplication.class, args);
+    }
+
+}
